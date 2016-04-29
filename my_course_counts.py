13804b0e258f2a31ea8b8d5a42574e0a7c88a85a
leahwinfrey <lwinfrey@oxy.edu>
2016-04-29 16:01:44 -0700

    TABLE CHANGES. PLZ BE DONE OMG

--- my_course_counts.py
+++ my_course_counts.py
@@ -337,10 +337,6 @@ def view_yeardepartsem(specyear, specsem, specdepart):
     return render_template('listyearsemesterdepartment.html', cursem=yeardepsem, years=YEAR_ABBREV[specyear],
                            depart=DEPART_ABBREV[specdepart], specsem=SEASONS[specsem])
 
-
-
-
-
 # The functions below lets you access files in the css, js, and images folders.
 # You should not change them unless you know what you are doing.
 
